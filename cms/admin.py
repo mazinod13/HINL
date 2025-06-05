872ac43ecae1b9029f5b281a52cfbfda7f00a523
Mishan1810 <mishanrajbhandari@gmail.com>
2025-06-05 17:14:16 +0545

    unique id and formula remains

--- cms/admin.py
+++ cms/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import EntrySheet
+from .models import EntrySheet,DashboardEntry
 
 admin.site.register(EntrySheet)
+admin.site.register(DashboardEntry)
 
